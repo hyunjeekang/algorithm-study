81e4e66438e0943f0092bf32d9b9f13fefe7c8aa
baappul <hihihyunjj@gmail.com>
2026-01-31 17:01:39 +0900

    ♻️ refactor: [BOJ] 12927 배수 스위치 - set 활용

--- hyunjeekang/BOJ/12927.py
+++ hyunjeekang/BOJ/12927.py
@@ -1,11 +1,19 @@
-lamps = [0] + [1 if x == 'Y' else 0 for x in input().strip()]
-n = len(lamps)
-cnt = 0
+import sys
 
-for i in range(1, n):
-    if lamps[i] == 1:
-        cnt += 1
-        for j in range(i, n, i):
-            lamps[j] = 1 - lamps[j]
+input_data = sys.stdin.readline().strip()
+N = len(input_data)
 
-print(cnt)
+on_lamps = {i for i, state in enumerate(input_data, 1) if state == 'Y'}
+ans = 0
+
+for i in range(1, N + 1):
+    if i in on_lamps:
+        ans += 1
+        
+        for j in range(i, N + 1, i):
+            if j in on_lamps:
+                on_lamps.remove(j)
+            else:
+                on_lamps.add(j)
+
+print(ans)
