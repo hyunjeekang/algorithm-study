846d1d38416233ee8224dc997bf74b1c68e61a04
jsy002514 <jsy002514@naver.com>
2026-02-04 00:14:17 +0900

    ✨feat: [SWEA] 4837 부분집합의 합

--- jeongsiyeong/SWEA/4836.py
+++ jeongsiyeong/SWEA/4836.py
@@ -1,21 +1,24 @@
-# SWEA 4836 색칠하기
+T = int(input())
 
-t = int(input())
-
-for test_case in range(1, t + 1):
-    n = int(input())
-    grid = [[(0,0)] * 10 for _ in range(10)]
-    for i in range(n):
-        a_x, a_y, b_x, b_y, color = map(int, input().split())
-        for x in range(a_x, b_x):
-            for y in range(a_y, b_y):
-                if color == 1:
-                    grid[x][y] = (grid[x][y][0] + 1, grid[x][y][1])
-                else:
-                    grid[x][y] = (grid[x][y][0], grid[x][y][1] + 1)
+for test_case in range(1, T + 1):
+    N = int(input())
+    # 10x10 격자를 0으로 초기화
+    grid = [[0] * 10 for _ in range(10)]
+    
+    for _ in range(N):
+        # r: row(행, y축 개념), c: col(열, x축 개념)
+        r1, c1, r2, c2, color = map(int, input().split())
+        
+        # 범위 끝점(+1)까지 포함하여 색칠
+        for r in range(r1, r2 + 1):
+            for c in range(c1, c2 + 1):
+                grid[r][c] += color  # 1(빨강) 또는 2(파랑)를 누적해서 더함
+                
+    # 격자에서 값이 3(빨강+파랑)인 칸의 개수를 카운트
     result = 0
-    for x in range(10):
-        for y in range(10):
-            if grid[x][y][0] > 0 and grid[x][y][1] > 0:
+    for r in range(10):
+        for c in range(10):
+            if grid[r][c] == 3:
                 result += 1
+                
     print(f"#{test_case} {result}")
