7363011a9eab587315b0525814e585f3d4735873
jsy002514 <jsy002514@naver.com>
2026-02-06 16:55:46 +0900

    ✨feat: [BOJ] 10026 적록색약

--- jeongsiyeong/BOJ/10026.py
+++ jeongsiyeong/BOJ/10026.py
@@ -1,92 +1,56 @@
 from collections import deque
-n = int(input())
 
+n = int(input())
 arr = [list(input()) for _ in range(n)]
 
-n_count = 0
-m_count = 0
-
-r_count = 0
-g_count = 0
-b_count = 0
-rg_count = 0
 visited = [[False]*n for _ in range(n)]
 
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
-def is_range(r, c):
-    return 0<=r<n and 0<=c<n
-def refresh():
-    global visited
-    visited = [[False]*n for _ in range(n)]
+def bfs(r, c):
+    q = deque()
+    q.append((r, c))
+    visited[r][c] = True
+    current_color = arr[r][c] # 시작점의 색상
 
-for r in range(n):
-    for c in range(n):
-        if not visited[r][c] and arr[r][c] == 'R':
-            q = deque()
-            q.append((r, c))
-            visited[r][c] = True
-            while q:
-                cur_r, cur_c = q.popleft()
-                
-                for dir in range(4):
-                    nr = cur_r + dr[dir]
-                    nc = cur_c + dc[dir]
-                    if is_range(nr, nc):
-                        if not visited[nr][nc] and arr[nr][nc] == 'R':
-                            visited[nr][nc] = True
-                            q.append((nr, nc))
-            r_count+=1
-            refresh()
-        elif not visited[r][c] and arr[r][c] == 'B':
-            q = deque()
-            q.append((r, c))
-            visited[r][c] = True
-            while q:
-                cur_r, cur_c = q.popleft()
-                
-                for dir in range(4):
-                    nr = cur_r + dr[dir]
-                    nc = cur_c + dc[dir]
-                    if is_range(nr, nc):
-                        if not visited[nr][nc] and arr[nr][nc] == 'B':
-                            visited[nr][nc] = True
-                            q.append((nr, nc))
-            b_count+=1
-            refresh()
-        elif not visited[r][c] and arr[r][c] == 'G':
-            q = deque()
-            q.append((r, c))
-            visited[r][c] = True
-            while q:
-                cur_r, cur_c = q.popleft()
-                
-                for dir in range(4):
-                    nr = cur_r + dr[dir]
-                    nc = cur_c + dc[dir]
-                    if is_range(nr, nc):
-                        if not visited[nr][nc] and arr[nr][nc] == 'G':
-                            visited[nr][nc] = True
-                            q.append((nr, nc))
-            g_count+=1
-            refresh()
-        if not visited[r][c] and (arr[r][c] == 'G' or arr[r][c] == 'R') :
-            q = deque()
-            q.append((r, c))
-            visited[r][c] = True
-            while q:
-                cur_r, cur_c = q.popleft()
-                
-                for dir in range(4):
-                    nr = cur_r + dr[dir]
-                    nc = cur_c + dc[dir]
-                    if is_range(nr, nc):
-                        if not visited[nr][nc] and (arr[r][c] == 'G' or arr[r][c] == 'R'):
-                            visited[nr][nc] = True
-                            q.append((nr, nc))
-            rg_count+=1
-            refresh()    
-n_count = r_count + g_count + b_count
-m_count = b_count + rg_count    
-print(n_count, m_count)
+    while q:
+        cur_r, cur_c = q.popleft()
+        
+        for i in range(4):
+            nr = cur_r + dr[i]
+            nc = cur_c + dc[i]
+            
+            if 0 <= nr < n and 0 <= nc < n:
+                # 방문하지 않았고, 같은 색상이라면 큐에 추가
+                if not visited[nr][nc] and arr[nr][nc] == current_color:
+                    visited[nr][nc] = True
+                    q.append((nr, nc))
+
+# 1. 적록색약이 아닌 경우 (Normal)
+normal_cnt = 0
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(i, j)
+            normal_cnt += 1
+
+# 2. 적록색약인 경우를 위해 세팅 변경
+# 'G'를 모두 'R'로 변경하여 R과 G를 통합
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 'G':
+            arr[i][j] = 'R'
+
+# visited 배열 초기화 (refresh)
+visited = [[False]*n for _ in range(n)]
+
+# 3. 적록색약인 경우 (Blind) 탐색
+blind_cnt = 0
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(i, j)
+            blind_cnt += 1
+
+print(normal_cnt, blind_cnt)
