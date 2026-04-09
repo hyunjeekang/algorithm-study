c30abf3b1a6557f05040ecfabb65927f8ec1c5be
jsy002514 <jsy002514@naver.com>
2026-04-09 17:23:41 +0900

    ✨feat: [CTR] 크기가 N인 순열

--- jeongsiyeong/cpp/codetree/bochoong_02_01.cpp
+++ jeongsiyeong/cpp/codetree/bochoong_02_01.cpp
@@ -1,38 +1,38 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int N, M;
-int answer = -1;
+vector<int> arr;
+int max_xor = -1;
 
-void dfs(int idx, vector<int>& list) {
-  if (list.size() == M) {
-    int xor_sum = 0; 
-    for (auto l : list) {
-      xor_sum ^= l;
+void dfs(int idx, int cnt, int current_xor) {
+    if (cnt == M) {
+        max_xor = max(max_xor, current_xor);
+        return;
     }
-    answer = max(answer, xor_sum);
-    return;
-  }
 
-  for (int i = idx; i <= N; i++) {
-    list.push_back(i);
-    dfs(i + 1, list); 
-    list.pop_back();
-  }
+    for (int i = idx; i < N; i++) {
+        dfs(i + 1, cnt + 1, current_xor ^ arr[i]);
+    }
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  if (cin >> N >> M) {
-    vector<int> arr;
-    dfs(1, arr);
-    cout << answer << "\n";
-  }
-  
-  return 0;
+    if (cin >> N >> M) {
+        arr.resize(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+
+        dfs(0, 0, 0);
+
+        cout << max_xor << "\n";
+    }
+
+    return 0;
 }
