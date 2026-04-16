aa5f3d52e9d61c39bd5fbd196d0e9c4c65ee4845
hyunjeekang <hihihyunjj@gmail.com>
2026-04-16 22:59:30 +0900

    ✨feat: [BOJ] 9465 스티커

--- hyunjeekang/BOJ/BOJ9465.java
+++ hyunjeekang/BOJ/BOJ9465.java
@@ -1,28 +1,20 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class BOJ9465 {
-    
-    static int T, N;
-    
-    static StringBuilder sb = new StringBuilder();
 
-
-    public static void main(String[] args)  throws IOException{
+    public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+        
         for(int t = 0; t < T; t++){
-            
-            N = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());
             int[][] s = new int[2][N];
             int[][] m = new int[2][N];
-
-            for(int i = 0 ; i < 2; i++){
-                Arrays.fill(m[i], -1);
-            }
 
             for(int r = 0; r < 2; r++){
                 st = new StringTokenizer(br.readLine());
@@ -31,24 +23,17 @@ public class BOJ9465 {
                 }
             }
 
-            int result = Math.max(dp(s, m, 0, N-1), dp(s, m, 1, N-1));
+            m[0][0] = s[0][0];
+            m[1][0] = s[1][0];
+
+            for (int c = 1; c < N; c++) {
+                m[0][c] = Math.max(m[1][c-1] + s[0][c], m[0][c-1]);
+                m[1][c] = Math.max(m[0][c-1] + s[1][c], m[1][c-1]);
+            }
+
+            int result = Math.max(m[0][N-1], m[1][N-1]);
             sb.append(result).append("\n");
         }
         System.out.println(sb);
-    }
-
-    private static int dp(int[][] s, int[][] m, int r, int c){
-        
-        if(c < 0) return 0;
-
-        if(c == 0) return s[r][c];
-
-        if(m[r][c] != -1) return m[r][c];
-
-        // 선택 안 하고 주변값가져오는 경우 추가해야함
-        if(r == 0) m[r][c] = dp(s, m, r+1, c-1) + s[r][c];
-        else m[r][c] = dp(s, m, r-1, c-1) + s[r][c];
-        
-        return m[r][c];
     }
 }
