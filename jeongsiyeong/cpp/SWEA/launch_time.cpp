4fb1d2c737ffab42791a6f498773c8ae28f23ce0
jsy002514 <jsy002514@naver.com>
2026-05-04 15:08:55 +0900

    refactor: 코드 최적화

--- jeongsiyeong/cpp/SWEA/launch_time.cpp
+++ jeongsiyeong/cpp/SWEA/launch_time.cpp
@@ -1,90 +1,100 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<queue>
-#include<tuple>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
 using namespace std;
 
-int get_dist(int sr, int sc, int pr, int pc) {
-	return abs(pr - sr) + abs(pc - sc);
-}
+int get_stair_time(int* team_dists, int sz, int stair_len) {
+    if (sz == 0) return 0;
 
-int get_stair_time(vector<int>& team_dists, int stair_len) {
-	if (team_dists.empty()) {
-		return 0;
-	}
+    sort(team_dists, team_dists + sz);
 
-	sort(team_dists.begin(), team_dists.end());
+    int q[10]; 
+    int head = 0;
+    int tail = 0;
 
-	queue<int> q;
+    for (int i = 0; i < sz; i++) {
+        int arrival_time = team_dists[i] + 1;
 
-	for (int dist : team_dists) {
-		int arrival_time = dist + 1;
-
-		if (q.size() < 3) {
-			q.push(arrival_time + stair_len);
-		}
-		else {
-			int prev_finish_time = q.front();
-			q.pop();
-			q.push(max(arrival_time, prev_finish_time) + stair_len);
-		}
-	}
-	return q.back();
+        if (tail - head < 3) {
+            q[tail++] = arrival_time + stair_len;
+        } 
+        else {
+            int prev_finish_time = q[head++];
+            q[tail++] = max(arrival_time, prev_finish_time) + stair_len;
+        }
+    }
+    return q[tail - 1]; 
 }
 
 int main() {
-	int T;
-	cin >> T;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	for (int test_case = 1; test_case <= T; test_case++) {
-		int N;
-		cin >> N;
+    int T;
+    cin >> T;
 
-		vector<pair<int, int>> persons;
-		vector<tuple<int, int, int>> stairs;
+    for (int test_case = 1; test_case <= T; test_case++) {
+        int N;
+        cin >> N;
 
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				int a;
-				cin >> a;
-				if (a == 1) {
-					persons.push_back({ r, c });
-				}
-				else if(a > 1) {
-					stairs.push_back(make_tuple( r, c, a ));
-				}
-			}
-		}
+        int persons[10][2];
+        int p_idx = 0;
 
-		int num_persons = persons.size();
-		int answer = 1e9;
+        int stairs[2][3];
+        int s_idx = 0;
 
-		for (int mask = 0; mask < (1 << num_persons); mask++) {
-			vector<int> team1_dists;
-			vector<int> team2_dists;
-			int sr1, sc1, slen1;
-			int sr2, sc2, slen2;
-			tie(sr1, sc1, slen1) = stairs[0];
-			tie(sr2, sc2, slen2) = stairs[1];
-			for (int i = 0; i < num_persons; i++) {
-				int pr = persons[i].first;
-				int pc = persons[i].second;
-				
-				if (mask & (1 << i)) {
-					team2_dists.push_back(get_dist(sr2, sc2, pr, pc));
-				}
-				else {
-					team1_dists.push_back(get_dist(sr1, sc1, pr, pc));
-				}
-			}
-			int time1 = get_stair_time(team1_dists, slen1);
-			int time2 = get_stair_time(team2_dists, slen2);
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                int val;
+                cin >> val;
+                if (val == 1) {
+                    persons[p_idx][0] = r;
+                    persons[p_idx][1] = c;
+                    p_idx++;
+                } else if (val > 1) {
+                    stairs[s_idx][0] = r;
+                    stairs[s_idx][1] = c;
+                    stairs[s_idx][2] = val;
+                    s_idx++;
+                }
+            }
+        }
 
-			int current_case_time = max(time1, time2);
+        int dist[10][2];
+        for (int i = 0; i < p_idx; i++) {
+            dist[i][0] = abs(persons[i][0] - stairs[0][0]) + abs(persons[i][1] - stairs[0][1]);
+            dist[i][1] = abs(persons[i][0] - stairs[1][0]) + abs(persons[i][1] - stairs[1][1]);
+        }
 
-			answer = min(answer, current_case_time);
-		}
-		cout << "#" << test_case << " " << answer << "\n";
-	}
+        int answer = 1e9;
+        int total_cases = 1 << p_idx;
+
+        for (int mask = 0; mask < total_cases; mask++) {
+            
+            int team1[10]; int sz1 = 0;
+            int team2[10]; int sz2 = 0;
+
+            for (int i = 0; i < p_idx; i++) {
+                if (mask & (1 << i)) {
+                    team2[sz2++] = dist[i][1];
+                } else {
+                    team1[sz1++] = dist[i][0];
+                }
+            }
+
+            int time1 = get_stair_time(team1, sz1, stairs[0][2]);
+            int time2 = get_stair_time(team2, sz2, stairs[1][2]);
+
+            int current_case_time = max(time1, time2);
+
+            if (current_case_time < answer) {
+                answer = current_case_time;
+            }
+        }
+
+        cout << "#" << test_case << " " << answer << "\n";
+    }
+    
+    return 0;
 }
